ddf1e30c1140175bdfb60e8ece9aca7b2792da50
bouni <bouni@owee.de>
2021-08-05 14:20:58 +0200

    setup logger under tabs

--- plugin.py
+++ plugin.py
@@ -10,8 +10,7 @@ import wx
 from pcbnew import *
 
 from .fabrication import JLCPCBFabrication
-
-# from .library import JLCPCBLibrary
+from .library import JLCPCBLibrary
 
 
 class JLCPCBPlugin(ActionPlugin):
@@ -38,9 +37,6 @@ class FabricationTab(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
 
-        log = wx.TextCtrl(
-            self, wx.ID_ANY, style=wx.TE_MULTILINE | wx.TE_READONLY, size=(-1, 400)
-        )
         description = wx.StaticText(
             self, label="Generate JLCPCB production and assembly files."
         )
@@ -52,15 +48,12 @@ class FabricationTab(wx.Panel):
         layout = wx.BoxSizer(wx.VERTICAL)
         layout.Add(description, flag=wx.EXPAND | wx.TOP | wx.LEFT | wx.RIGHT, border=5)
         layout.Add(buttonSizer, flag=wx.EXPAND | wx.TOP | wx.LEFT | wx.RIGHT, border=5)
-        layout.Add(
-            log, flag=wx.EXPAND | wx.BOTTOM | wx.TOP | wx.LEFT | wx.RIGHT, border=5
-        )
+        # layout.Add(
+        #     log, flag=wx.EXPAND | wx.BOTTOM | wx.TOP | wx.LEFT | wx.RIGHT, border=5
+        # )
         self.SetSizer(layout)
         self.Refresh()
         self.Layout()
-        # redirect text here
-        sys.stdout = log
-        self.init_logger()
 
     def generate(self, e):
         """Generate Fabrication data."""
@@ -73,29 +66,48 @@ class FabricationTab(wx.Panel):
         self.fabrication.generate_cpl()
         self.fabrication.generate_bom()
 
-    def init_logger(self):
-        # Remove all handlers associated with the root logger object.
-        for handler in logging.root.handlers[:]:
-            logging.root.removeHandler(handler)
-
-        root = logging.getLogger()
-        root.setLevel(logging.DEBUG)
-
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y.%m.%d %H:%M:%S"
-        )
-        handler.setFormatter(formatter)
-        root.addHandler(handler)
-        self.logger = logging.getLogger(__name__)
-        self.logger.info("Test")
-
 
 class LibraryTab(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent)
-        t = wx.StaticText(self, -1, "This is the second tab", (20, 20))
+        description = wx.StaticText(
+            self, label="Generate JLCPCB production and assembly files."
+        )
+        self.download_button = wx.Button(self, label="Update library", size=(200, -1))
+        self.download_button.Bind(wx.EVT_BUTTON, self.download)
+        self.ready = False
+
+        buttonSizer = wx.BoxSizer(wx.HORIZONTAL)
+        buttonSizer.Add(
+            self.download_button, flag=wx.EXPAND | wx.TOP | wx.LEFT | wx.RIGHT, border=5
+        )
+        layout = wx.BoxSizer(wx.VERTICAL)
+        layout.Add(description, flag=wx.EXPAND | wx.TOP | wx.LEFT | wx.RIGHT, border=5)
+        layout.Add(buttonSizer, flag=wx.EXPAND | wx.TOP | wx.LEFT | wx.RIGHT, border=5)
+        self.SetSizer(layout)
+        self.Refresh()
+        self.Layout()
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("Library")
+        self.library = JLCPCBLibrary()
+
+    def setup(self):
+        if not os.path.isfile(self.library.xls):
+            wx.MessageBox("No library found, will download it now!")
+            self.download()
+        wx.MessageBox("Load library data, please wait ...")
+        self.status_text.SetLabel(f"Load data, please wait ...")
+        self.library.load_data(self.status_text)
+        self.status_text.SetLabel(f"Library ready")
+        self.ready = True
+
+    def download(self, e):
+        """Download latest library data."""
+        e.Skip()
+        # self.status_text.SetLabel("Downloading, please wait ...")
+        # self.library.download()
+        # self.status_text.SetLabel(f"Library ready, {self.library.partcount} entries")
+        # wx.MessageBox("Download of latest library finished!")
 
 
 class Dialog(wx.Dialog):
@@ -118,18 +130,44 @@ class Dialog(wx.Dialog):
                 )
             )
         )
-
         panel = wx.Panel(self)
-        notebook = wx.Notebook(panel)
+        # Important to setup logger here before tabs are created so that we can log from every sub window
+        log = wx.TextCtrl(
+            panel, wx.ID_ANY, style=wx.TE_MULTILINE | wx.TE_READONLY, size=(-1, 200)
+        )
+        # redirect text here
+        sys.stdout = log
+        self.init_logger()
 
+        notebook = wx.Notebook(panel)
         fabrication_tab = FabricationTab(notebook)
         library_tab = LibraryTab(notebook)
-
         notebook.AddPage(fabrication_tab, "Fabrication data")
         notebook.AddPage(library_tab, "Parts library")
-        sizer = wx.BoxSizer()
+
+        sizer = wx.BoxSizer(wx.VERTICAL)
         sizer.Add(notebook, 1, wx.EXPAND)
+        sizer.Add(log, 1, wx.EXPAND)
         panel.SetSizer(sizer)
+
+        self.Show()
+
+    def init_logger(self):
+        # Remove all handlers associated with the root logger object.
+        for handler in logging.root.handlers[:]:
+            logging.root.removeHandler(handler)
+
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
+
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y.%m.%d %H:%M:%S"
+        )
+        handler.setFormatter(formatter)
+        root.addHandler(handler)
+        self.logger = logging.getLogger(__name__)
 
 
 JLCPCBPlugin().register()
