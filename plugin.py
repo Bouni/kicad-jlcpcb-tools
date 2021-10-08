15180ebd0bc075498a8334859a2e0440b49691e8
Daniel O'Connor <darius@dons.net.au>
2021-10-08 23:35:51 +1030

    Make the part detail display non-modal.

--- plugin.py
+++ plugin.py
@@ -340,7 +340,7 @@ class JLCBCBTools(wx.Dialog):
         part = self.footprint_list.GetTextValue(row, 3)
         if part != "":
             dialog = PartDetailsDialog(self, part)
-            result = dialog.ShowModal()
+            dialog.Show()
 
     def init_logger(self):
         """Initialize logger to log into textbox"""
@@ -655,7 +655,7 @@ class PartSelectorDialog(wx.Dialog):
             return
         part = self.part_list.GetTextValue(row, 0)
         dialog = PartDetailsDialog(self, part)
-        result = dialog.ShowModal()
+        dialog.Show()
 
 
 class PartDetailsDialog(wx.Dialog):
