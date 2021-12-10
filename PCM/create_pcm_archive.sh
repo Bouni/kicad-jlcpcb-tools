2d6c2207589d6ab61646b5fedf85a690780cc8df
bouni <bouni@owee.de>
2021-12-10 11:48:42 +0000

    pass archive data to custom kicad addon repo

--- PCM/create_pcm_archive.sh
+++ PCM/create_pcm_archive.sh
@@ -7,7 +7,7 @@ VERSION=$1
 echo "Clean up old files"
 rm -f PCM/*.zip
 rm -rf PCM/archive
-rm -f PCM/metadata.json
+#rm -f PCM/metadata.json
 
 
 echo "Create folder structure for ZIP"
@@ -20,7 +20,7 @@ cp *.png PCM/archive/plugins
 cp -r icons PCM/archive/plugins
 cp PCM/icon.png PCM/archive/resources
 cp PCM/metadata.template.json PCM/archive/metadata.json
-cp PCM/metadata.template.json PCM/metadata.json
+#cp PCM/metadata.template.json PCM/metadata.json
 
 echo "Modify archive metadata.json"
 sed -i "s/VERSION_HERE/$VERSION/g" PCM/archive/metadata.json
@@ -35,16 +35,16 @@ cd PCM/archive
 zip -r ../KiCAD-PCM-$VERSION.zip .
 cd ../..
 
-echo "Gather data for merge request metadata.json"
-DOWNLOAD_SHA256=$(shasum --algorithm 256 PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f1)
-DOWNLOAD_SIZE=$(ls -l PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f5)
-DOWNLOAD_URL="https:\/\/github.com\/Bouni\/kicad-jlcpcb-tools\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip"
-INSTALL_SIZE=$(unzip -l PCM/KiCAD-PCM-$VERSION.zip | tail -1 | xargs | cut -d' ' -f1)
+echo "Gather data for repo rebuild"
+export DOWNLOAD_SHA256=$(shasum --algorithm 256 PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f1)
+export DOWNLOAD_SIZE=$(ls -l PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f5)
+export DOWNLOAD_URL="https:\/\/github.com\/Bouni\/kicad-jlcpcb-tools\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip"
+export INSTALL_SIZE=$(unzip -l PCM/KiCAD-PCM-$VERSION.zip | tail -1 | xargs | cut -d' ' -f1)
 
-echo "Modify merge request metadata.json"
-sed -i "s/VERSION_HERE/$VERSION/g" PCM/metadata.json
-sed -i "s/SHA256_HERE/$DOWNLOAD_SHA256/g" PCM/metadata.json
-sed -i "s/DOWNLOAD_SIZE_HERE/$DOWNLOAD_SIZE/g" PCM/metadata.json
-sed -i "s/DOWNLOAD_URL_HERE/$DOWNLOAD_URL/g" PCM/metadata.json
-sed -i "s/INSTALL_SIZE_HERE/$INSTALL_SIZE/g" PCM/metadata.json
+#echo "Modify merge request metadata.json"
+#sed -i "s/VERSION_HERE/$VERSION/g" PCM/metadata.json
+#sed -i "s/SHA256_HERE/$DOWNLOAD_SHA256/g" PCM/metadata.json
+#sed -i "s/DOWNLOAD_SIZE_HERE/$DOWNLOAD_SIZE/g" PCM/metadata.json
+#sed -i "s/DOWNLOAD_URL_HERE/$DOWNLOAD_URL/g" PCM/metadata.json
+#sed -i "s/INSTALL_SIZE_HERE/$INSTALL_SIZE/g" PCM/metadata.json
 
