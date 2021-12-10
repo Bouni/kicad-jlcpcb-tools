c6821ec1e79533e75c62564ce94020bdf17045b2
bouni <bouni@owee.de>
2021-12-10 12:53:50 +0000

    fixed env var mistake

--- PCM/create_pcm_archive.sh
+++ PCM/create_pcm_archive.sh
@@ -36,7 +36,7 @@ zip -r ../KiCAD-PCM-$VERSION.zip .
 cd ../..
 
 echo "Gather data for repo rebuild"
-echo DOWNLOAD_SHA256=$(shasum --algorithm 256 PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f1)
+echo DOWNLOAD_SHA256=$(shasum --algorithm 256 PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f1) >> $GITHUB_ENV
 echo DOWNLOAD_SIZE=$(ls -l PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f5) >> $GITHUB_ENV
 echo DOWNLOAD_URL="https:\/\/github.com\/Bouni\/kicad-jlcpcb-tools\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip" >> $GITHUB_ENV
 echo INSTALL_SIZE=$(unzip -l PCM/KiCAD-PCM-$VERSION.zip | tail -1 | xargs | cut -d' ' -f1) >> $GITHUB_ENV
