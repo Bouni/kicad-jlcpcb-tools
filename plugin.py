c986fe7aa6dc14ec771795cf1621fa09521c25ac
bouni <bouni@owee.de>
2022-05-04 08:04:24 +0200

    Refactored as shown in the latest ActionPlugins dev docs

--- plugin.py
+++ plugin.py
@@ -6,17 +6,16 @@ from .mainwindow import JLCPCBTools
 
 
 class JLCPCBPlugin(ActionPlugin):
-    def __init__(self):
-        super(JLCPCBPlugin, self).__init__()
+    def defaults(self):
         self.name = "JLCPCB Tools"
         self.category = "Fabrication data generation"
-        self.pcbnew_icon_support = hasattr(self, "show_toolbar_button")
-        self.show_toolbar_button = True
-        path, filename = os.path.split(os.path.abspath(__file__))
-        self.icon_file_name = os.path.join(path, "jlcpcb-icon.png")
         self.description = (
             "Generate JLCPCB-compatible Gerber, Excellon, BOM and CPL files"
         )
+        self.show_toolbar_button = True
+        path, filename = os.path.split(os.path.abspath(__file__))
+        self.icon_file_name = os.path.join(path, "jlcpcb-icon.png")
+        self._pcbnew_frame = None
 
     def Run(self):
         dialog = JLCPCBTools(None)
