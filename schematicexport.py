c291b112a6300657e3c817d01372281378eadcac
Scott Hanson <scooter_seh@yahoo.com>
2022-08-25 17:28:54 -0400

    schematic export fixes, skip symbol library part section, fix single digit XY positions, fix add/replace logic to prevent duplicate LCSC fields, added more logging

--- schematicexport.py
+++ schematicexport.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import os.path
 import re
 
 import wx
@@ -20,31 +21,39 @@ class SchematicExport:
             self._update_schematic(path)
 
     def _update_schematic(self, path):
+        self.logger.info(f"Reading {path}...")
         """Regex to look through schematic property, if we hit the pin section without finding a LCSC property, add it"""
         """keep track of property ids and Reference property location to use with new LCSC property"""
         propRx = re.compile(
-            '\\(property\\s\\"(.*)\\"\s\\"(.*)\\"\s\\(id\\s(\\d+)\\)\\s\\(at\\s(-?\\d+.\\d+\\s-?\\d+.\\d+)\s\\d+\\)'
+            '\\(property\\s\\"(.*)\\"\s\\"(.*)\\"\s\\(id\\s(\\d+)\\)\\s\\(at\\s(-?\\d+(?:.\\d+)?\\s-?\\d+(?:.\\d+)?)\s\\d+\\)'
         )
         pinRx = re.compile('\\(pin\\s\\"(.*)\\"\\s\\(')
 
         store_parts = self.parent.store.read_all()
 
-        lastID = 0
+        lastID = -1
         lastLoc = ""
         lastLcsc = ""
         newLcsc = ""
+        lastRef = ""
 
         lines = []
         newlines = []
         with open(path) as f:
             lines = f.readlines()
 
+        if os.path.exists(path + "_old"):
+            os.remove(path + "_old")
         os.rename(path, path + "_old")
+        partSection = False
+
         for line in lines:
             inLine = line.rstrip()
             outLine = inLine
+            if "(symbol (lib_id" in inLine: #skip library section
+                partSection = True
             m = propRx.search(inLine)
-            if m:
+            if m and partSection:
                 key = m.group(1)
                 value = m.group(2)
                 lastID = int(m.group(3))
@@ -52,11 +61,14 @@ class SchematicExport:
                 # found a LCSC property, so update it if needed
                 if key == "LCSC":
                     lastLcsc = value
-                    if lastLcsc == newLcsc:
-                        outLine.replace(lastLcsc, newLcsc)
+                    if lastLcsc != newLcsc and newLcsc != "":
+                        self.logger.info(f"Updating {newLcsc} on {lastRef}")
+                        outLine = outLine.replace("\"" + lastLcsc + "\"", "\"" + newLcsc + "\"")
+                        lastLcsc = newLcsc
 
                 if key == "Reference":
                     lastLoc = m.group(4)
+                    lastRef = value
                     for part in store_parts:
                         if value == part[0]:
                             newLcsc = part[3]
@@ -64,18 +76,19 @@ class SchematicExport:
             # if we hit the pin section without finding a LCSC property, add it
             m = pinRx.search(inLine)
             if m:
-                if lastLcsc == "" and newLcsc != "" and lastLoc != "" and lastID != 0:
-                    self.logger.info(f"added {newLcsc}")
+                if lastLcsc == "" and newLcsc != "" and lastLoc != "" and lastID != -1:
+                    self.logger.info(f"added {newLcsc} to {lastRef}")
                     newTxt = '    (property "LCSC" "{}" (id {}) (at {} 0)'.format(
                         newLcsc, (lastID + 1), lastLoc
                     )
                     newlines.append(newTxt)
                     newlines.append("      (effects (font (size 1.27 1.27)) hide)")
                     newlines.append("    )")
-                lastID = 0
+                lastID = -1
                 lastLoc = ""
                 lastLcsc = ""
                 newLcsc = ""
+                lastRef = ""
             newlines.append(outLine)
 
         f = open(path, "w")
