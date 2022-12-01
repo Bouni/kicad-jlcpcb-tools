28b7834ad6b8141f4a1853884e78acadfb60a72c
Bartek Wacławik <bartek.waclawik@student.uj.edu.pl>
2022-12-01 22:30:38 +0100

    Make part selector assign multiple parts at once

--- events.py
+++ events.py
@@ -3,5 +3,5 @@ from wx.lib.newevent import NewEvent
 ResetGaugeEvent, EVT_RESET_GAUGE_EVENT = NewEvent()
 UpdateGaugeEvent, EVT_UPDATE_GAUGE_EVENT = NewEvent()
 MessageEvent, EVT_MESSAGE_EVENT = NewEvent()
-AssignPartEvent, EVT_ASSIGN_PART_EVENT = NewEvent()
+AssignPartsEvent, EVT_ASSIGN_PARTS_EVENT = NewEvent()
 PopulateFootprintListEvent, EVT_POPULATE_FOOTPRINT_LIST_EVENT = NewEvent()
