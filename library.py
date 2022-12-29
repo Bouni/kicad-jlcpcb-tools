459f840aed2f3b6b86673d260fe81d00b8ed8e34
Bouni <bouni@owee.de>
2022-12-29 13:11:58 +0100

    fix rename issue for backup of parts.db

--- library.py
+++ library.py
@@ -380,7 +380,7 @@ class Library:
                 return
         # rename existing parts.db
         if os.path.exists(self.dbfile):
-            os.rename(self.dbfile, f"{self.dbfile}.bak")
+            os.rename(self.dbfile, f"{self.dbfile}-{dt.now().strftime('%Y-%m-%d-%H-%M-%S')}.bak")
         # unzip downloaded parts.zip
         with zipfile.ZipFile(os.path.join(self.datadir, "parts.zip"), "r") as z:
             z.extractall(self.datadir)
