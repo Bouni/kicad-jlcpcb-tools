0858f74cf1385b1573a93a735fa6a6bbef658ff0
Bouni <bouni@owee.de>
2023-01-09 13:55:11 +0100

    Add better visualisation for settings

--- settings.py
+++ settings.py
@@ -55,29 +55,53 @@ class SettingsDialog(wx.Dialog):
             wx.ToolTip("Whether vias should be coverd by soldermask or not")
         )
 
+        self.tented_vias_image = wx.StaticBitmap(
+            self,
+            wx.ID_ANY,
+            loadBitmapScaled("tented.png", self.parent.scale_factor, static=True),
+            wx.DefaultPosition,
+            wx.DefaultSize,
+            0
+        )
+
         self.tented_vias_setting.Bind(wx.EVT_CHECKBOX, self.update_settings)
+
+        tented_vias_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        tented_vias_sizer.Add(self.tented_vias_image, 10, wx.ALL | wx.EXPAND, 5)
+        tented_vias_sizer.Add(self.tented_vias_setting, 100, wx.ALL | wx.EXPAND, 5)
 
         # ---------------------------------------------------------------------
         # ---------------------- Main Layout Sizer ----------------------------
         # ---------------------------------------------------------------------
 
         layout = wx.GridSizer(10, 2, 0, 0)
-        layout.Add(self.tented_vias_setting, 0, wx.ALL | wx.EXPAND, 10)
-
+        layout.Add(tented_vias_sizer, 0, wx.ALL | wx.EXPAND, 5)
+        
         self.SetSizer(layout)
         self.Layout()
         self.Centre(wx.BOTH)
 
         self.load_settings()
 
+    def update_tented_vias(self, tented):
+        """Update settings dialog according to the settings."""
+        if tented:
+            self.tented_vias_setting.SetValue(tented)
+            self.tented_vias_setting.SetLabel("Tented vias")
+            self.tented_vias_image.SetBitmap(loadBitmapScaled("tented.png", self.parent.scale_factor, static=True))
+        else:
+            self.tented_vias_setting.SetValue(tented)
+            self.tented_vias_setting.SetLabel("Untented vias")
+            self.tented_vias_image.SetBitmap(loadBitmapScaled("untented.png", self.parent.scale_factor, static=True))
+
     def load_settings(self):
         """Load settings and set checkboxes accordingly"""
-        self.tented_vias_setting.SetValue(
-            self.parent.settings.get("gerber", {}).get("tented_vias", True)
-        )
+        self.update_tented_vias(self.parent.settings.get("gerber", {}).get("tented_vias", True))
 
     def update_settings(self, event):
         """Update and persist a setting that was changed."""
+        upd = getattr(self, f"update_{event.GetEventObject().GetName()}")
+        upd(event.GetEventObject().GetValue())
         wx.PostEvent(
             self.parent,
             UpdateSetting(
