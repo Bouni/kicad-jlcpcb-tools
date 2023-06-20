d86f521282808b413ab494618d6601b9d6c0ec19
bouni <bouni@owee.de>
2023-06-20 08:56:47 +0200

    Toggle BOM/POS without re-populate entire list

---
+++ const.py
@@ -0,0 +1,14 @@
+from enum import IntEnum
+
+
+class Column(IntEnum):
+    REFERENCE = 0
+    VALUE = 1
+    FOOTPRINT = 2
+    LCSC = 3
+    TYPE = 4
+    STOCK = 5
+    BOM = 6
+    POS = 7
+    ROTATION = 8
+    SIDE = 9
