9292267aff76fbd8ef3ea11f077e03d57d6419ad
Bouni <bouni@owee.de>
2023-07-21 16:24:50 +0200

    Use _ for unused variable

--- plugin.py
+++ plugin.py
@@ -19,7 +19,7 @@ class JLCPCBPlugin(ActionPlugin):
             "Generate JLCPCB-compatible Gerber, Excellon, BOM and CPL files"
         )
         self.show_toolbar_button = True
-        path, filename = os.path.split(os.path.abspath(__file__))
+        path, _ = os.path.split(os.path.abspath(__file__))
         self.icon_file_name = os.path.join(path, "jlcpcb-icon.png")
         self._pcbnew_frame = None
 
