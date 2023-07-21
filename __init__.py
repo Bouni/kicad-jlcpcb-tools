6b50eb852eaa3c055e479924a58eeded320b7784
bouni <bouni@owee.de>
2023-07-21 09:09:48 +0000

    Remove comments and add pylint disable for module docstring

--- __init__.py
+++ __init__.py
@@ -1,22 +1,4 @@
-# import logging
-# import os
-
+# pylint: disable=missing-module-docstring
 from .plugin import JLCPCBPlugin
 
-# logging.basicConfig(
-#     level=logging.DEBUG,
-#     format="%(asctime)s [%(levelname)s] %(message)s",
-#     handlers=[
-#         logging.FileHandler(
-#             os.path.join(os.path.dirname(os.path.realpath(__file__)), "debug.log")
-#         ),
-#         logging.StreamHandler(),
-#     ],
-# )
-
-# LOGGER = logging.getLogger()
-
-# try:
 JLCPCBPlugin().register()
-# except Exception as e:
-# LOGGER.debug(repr(e))
