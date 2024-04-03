99eac5fc5e6f2385fbe0b049f227291be4d493d7
Chris Morgan <chmorgan@gmail.com>
2024-04-02 21:48:26 -0400

    Standalone mode - Run the plugin outside of KiCad
    Enables debugging of the plugin UI with PyCharm/vscode
    
    - see __main__.py for instructions on how to launch
    - Wrap KiCad calls with stubs
    
    Based on the idea and initial PR authored by Dominic Cerquetti <binary1230@users.noreply.github.com>.

---
+++ __main__.py
@@ -0,0 +1,19 @@
+"""Entry point for running the plugin in standalone mode."""
+
+import wx
+
+from . import standalone_impl
+from .mainwindow import JLCPCBTools
+
+if __name__ == "__main__":
+    print("starting jlcpcbtools standalone mode...") # noqa: T201
+
+    # See README.md for how to use this
+
+    app = wx.App(None)
+
+    dialog = JLCPCBTools(None, kicad_provider=standalone_impl.KicadStub())
+    dialog.Center()
+    dialog.Show()
+
+    app.MainLoop()
