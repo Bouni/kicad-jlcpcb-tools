b5a9a4de953ab2636f29e9538aee835f2231ccc3
Chris Morgan <chmorgan@gmail.com>
2025-06-09 22:13:21 -0400

    __init__.py - Add 'lib' folder to module search path to find the modules installed with this plugin

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
 """Init file for plugin."""
 
-from .plugin import JLCPCBPlugin
+import os
+import sys
+
+lib_path = os.path.join(os.path.dirname(__file__), "lib")
+if lib_path not in sys.path:
+    sys.path.append(lib_path)
+
+from .plugin import JLCPCBPlugin  # noqa: I001, E402
 
 if __name__ != "__main__":
     JLCPCBPlugin().register()
