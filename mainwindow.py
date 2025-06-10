f027b0f0b24e0c60ee8d7224c85a57a14f6c5246
Bouni <bouni@owee.de>
2025-06-10 15:26:40 +0200

    Show warning if plausibility check fails

--- mainwindow.py
+++ mainwindow.py
@@ -965,6 +965,18 @@ class JLCPCBTools(wx.Dialog):
 
     def generate_fabrication_data(self, *_):
         """Generate fabrication data."""
+        warnings = self.fabrication.check_plausibility()
+        if warnings:
+            result = wx.MessageBox(
+                "There are items with identical LCSC number but different values in the list:\n"
+                + warnings
+                + "Continue?",
+                "Plausibility check",
+                wx.OK | wx.CANCEL | wx.CENTER,
+            )
+            if result == wx.CANCEL:
+                return
+
         if self.settings.get("general", {}).get("order_number"):
             count = self.count_order_number_placeholders()
             if count == 0:
