a2f28be65de615b2fe21ef82a71ca859077c5cd6
Andy Miller <z2amiller@gmail.com>
2026-05-01 23:47:41 -0700

    fix(settings): remove duplicate subtract_mask_from_silk merge blocks

--- settings.py
+++ settings.py
@@ -111,43 +111,6 @@ class SettingsDialog(wx.Dialog):
         fill_zones_sizer.Add(self.fill_zones_image, 10, wx.ALL | wx.EXPAND, 5)
         fill_zones_sizer.Add(self.fill_zones_setting, 100, wx.ALL | wx.EXPAND, 5)
 
-        ##### Subtract solder mask from silk ######
-
-        self.subtract_mask_from_silk_setting = wx.CheckBox(
-            self,
-            id=wx.ID_ANY,
-            label="Subtract solder mask from silk",
-            pos=wx.DefaultPosition,
-            size=wx.DefaultSize,
-            style=0,
-            name="gerber_subtract_mask_from_silk",
-        )
-
-        self.subtract_mask_from_silk_setting.SetToolTip(
-            wx.ToolTip(
-                "Whether silkscreen should be clipped around solder-mask openings"
-            )
-        )
-
-        self.subtract_mask_from_silk_image = wx.StaticBitmap(
-            self,
-            wx.ID_ANY,
-            loadBitmapScaled("plot_refs.png", self.parent.scale_factor, static=True),
-            wx.DefaultPosition,
-            wx.DefaultSize,
-            0,
-        )
-
-        self.subtract_mask_from_silk_setting.Bind(wx.EVT_CHECKBOX, self.update_settings)
-
-        subtract_mask_from_silk_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        subtract_mask_from_silk_sizer.Add(
-            self.subtract_mask_from_silk_image, 10, wx.ALL | wx.EXPAND, 5
-        )
-        subtract_mask_from_silk_sizer.Add(
-            self.subtract_mask_from_silk_setting, 100, wx.ALL | wx.EXPAND, 5
-        )
-
         ##### Force DRC before Gerber export #####
 
         self.force_drc_setting = wx.CheckBox(
@@ -712,7 +675,6 @@ class SettingsDialog(wx.Dialog):
         settings_grid = wx.GridSizer(0, 2, 0, 0)
         settings_grid.Add(tented_vias_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(fill_zones_sizer, 0, wx.ALL | wx.EXPAND, 5)
-        settings_grid.Add(subtract_mask_from_silk_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(force_drc_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(plot_values_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(plot_references_sizer, 0, wx.ALL | wx.EXPAND, 5)
@@ -945,18 +907,6 @@ class SettingsDialog(wx.Dialog):
                 "Do not highlight standard-mode trigger parts"
             )
 
-    def update_subtract_mask_from_silk(self, enabled):
-        """Update settings dialog according to mask/silk subtraction setting."""
-        self.subtract_mask_from_silk_setting.SetValue(bool(enabled))
-        if enabled:
-            self.subtract_mask_from_silk_setting.SetLabel(
-                "Subtract solder mask from silk"
-            )
-        else:
-            self.subtract_mask_from_silk_setting.SetLabel(
-                "Do not subtract solder mask from silk"
-            )
-
     def update_bom_estimator_show(self, show):
         """Update settings dialog according to the BOM estimator visibility setting."""
         self.bom_estimator_show_setting.SetValue(bool(show))
@@ -1013,9 +963,6 @@ class SettingsDialog(wx.Dialog):
             self.parent.settings.get("general", {}).get(
                 "highlight_standard_parts", True
             )
-        )
-        self.update_subtract_mask_from_silk(
-            self.parent.settings.get("gerber", {}).get("subtract_mask_from_silk", True)
         )
         self.update_bom_estimator_show(
             self.parent.settings.get("general", {}).get("bom_estimator_show", True)
