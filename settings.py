24806a837e6b93589c736e4a7c5074eafdd5c8ca
RolandW <roland.wache@rockwellautomation.com>
2026-08-14 15:00:05 +0200

    Add per-layer PCB PDF export

--- settings.py
+++ settings.py
@@ -296,6 +296,27 @@ class SettingsDialog(wx.Dialog):
         lcsc_bom_cpl_sizer.Add(self.lcsc_bom_cpl_image, 10, wx.ALL | wx.EXPAND, 5)
         lcsc_bom_cpl_sizer.Add(self.lcsc_bom_cpl_setting, 100, wx.ALL | wx.EXPAND, 5)
 
+        ##### Export PCB layer PDFs #####
+
+        self.export_pcb_layer_pdfs_setting = wx.CheckBox(
+            self,
+            id=wx.ID_ANY,
+            label="Export one PDF for each PCB layer",
+            pos=wx.DefaultPosition,
+            size=wx.DefaultSize,
+            style=0,
+            name="gerber_export_pcb_layer_pdfs",
+        )
+        self.export_pcb_layer_pdfs_setting.SetToolTip(
+            wx.ToolTip("Export one PDF file for each plotted PCB layer")
+        )
+        self.export_pcb_layer_pdfs_setting.Bind(wx.EVT_CHECKBOX, self.update_settings)
+
+        export_pcb_layer_pdfs_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        export_pcb_layer_pdfs_sizer.Add(
+            self.export_pcb_layer_pdfs_setting, 100, wx.ALL | wx.EXPAND, 5
+        )
+
         ##### Check if order/serial number placeholder is present #####
 
         self.order_number_setting = wx.CheckBox(
@@ -678,6 +699,7 @@ class SettingsDialog(wx.Dialog):
         settings_grid.Add(subtract_mask_from_silk_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(lcsc_priority_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(lcsc_bom_cpl_sizer, 0, wx.ALL | wx.EXPAND, 5)
+        settings_grid.Add(export_pcb_layer_pdfs_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(order_number_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(highlight_matches_sizer, 0, wx.ALL | wx.EXPAND, 5)
         settings_grid.Add(highlight_standard_parts_sizer, 0, wx.ALL | wx.EXPAND, 5)
@@ -856,6 +878,18 @@ class SettingsDialog(wx.Dialog):
                 loadBitmapScaled("no_bom.png", self.parent.scale_factor, static=True)
             )
 
+    def update_export_pcb_layer_pdfs(self, enabled):
+        """Update the PCB layer PDF export setting."""
+        self.export_pcb_layer_pdfs_setting.SetValue(bool(enabled))
+        if enabled:
+            self.export_pcb_layer_pdfs_setting.SetLabel(
+                "Export one PDF for each PCB layer"
+            )
+        else:
+            self.export_pcb_layer_pdfs_setting.SetLabel(
+                "Do not export PCB layer PDFs"
+            )
+
     def update_order_number(self, check):
         """Update settings dialog according to the settings."""
         self.logger.debug(check)
@@ -941,6 +975,9 @@ class SettingsDialog(wx.Dialog):
         )
         self.update_lcsc_bom_cpl(
             self.parent.settings.get("gerber", {}).get("lcsc_bom_cpl", True)
+        )
+        self.update_export_pcb_layer_pdfs(
+            self.parent.settings.get("gerber", {}).get("export_pcb_layer_pdfs", False)
         )
         self.update_order_number(
             self.parent.settings.get("general", {}).get("order_number", True)
